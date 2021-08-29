1d4d235836f36f51bf4ebef0a532837c739309ea
regaliv <regaliv@example.com>
2021-08-29 10:15:50 +0300

    coffe без  пароля

---
+++ app/src/main/java/android/example/cafeorder/CreateOrderActivity.java
@@ -0,0 +1,22 @@
+package android.example.cafeorder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class CreateOrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_order);
+    }
+
+    public void onClickChangeDrink(View view) {
+        
+    }
+
+    public void onClickSendOrder(View view) {
+    }
+}
